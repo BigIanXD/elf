78444ccaef7446302833813043bcfeb302b4e54e
BuildTools <unconfigured@null.spigotmc.org>
2024-01-02 21:19:01 +0800

    fixed a bug

--- src/script.js
+++ src/script.js
@@ -1,3 +1,61 @@
+// const canvas = document.getElementById("canvas");
+// const ctx = canvas.getContext("2d");
+
+// var keycode = {
+//     38: "up",
+// 	40: "down",
+// 	37: "left",
+// 	39: "right"
+// };
+
+// var move = function (dir, distance) {
+//     if (dir == "up" && this.y - distance > 0) {
+//         ctx.clearRect(this.x, this.y, 10, 10);
+//         this.y -= distance;
+//         this.draw();
+//     } else if (dir == "down" && this.y + distance + 10 < canvas.clientHeight) {
+//         ctx.clearRect(this.x, this.y, 10, 10);
+//         this.y += distance;
+//         this.draw();
+//     }
+//     else if (dir == "right" && this.x + distance < canvas.clientWidth) {
+//         ctx.clearRect(this.x, this.y, 10, 10);
+//         this.x += distance;
+//         this.draw();
+//     }
+//     else if (dir == "left" && this.x - distance > 0) {
+//         ctx.clearRect(this.x, this.y, 10, 10);
+//         this.x -= distance;
+//         this.draw();
+//     }
+// }
+
+// var draw_elf = function () {  
+//     ctx.drawImage(elf,this.x ,this.y , 10, 10);
+// }
+// var elf = new Image();
+// elf.src = "src\\elf_right.png";
+// var elf_val = {
+//     x : 10,
+//     y : 10,
+//     draw : draw_elf,
+//     move : move
+// };
+
+// var elf_Direction = "right";
+
+// var interval = function () { 
+//     var speed = 1;
+//     elf_val.move(elf_Direction, speed);
+// }
+
+// setInterval(interval, 25);
+
+// $("body").keydown(function (e) { 
+//     elf_Direction = keycode[e.keyCode];
+//     elf.src = "src\\elf_" + keycode[e.keyCode] + ".png"
+// });
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -8,52 +66,52 @@ var keycode = {
 	39: "right"
 };
 
-var move = function (dir, distance) {
+const move = function (dir, distance) {
     if (dir == "up" && this.y - distance > 0) {
-        ctx.clearRect(this.x, this.y, 10, 10);
+        ctx.clearRect(this.x, this.y, this.size, this.size);
         this.y -= distance;
         this.draw();
-    } else if (dir == "down" && this.y + distance < canvas.clientHeight) {
-        ctx.clearRect(this.x, this.y, 10, 10);
+    } else if (dir == "down" && this.y + distance + 10 < canvas.height) {
+        ctx.clearRect(this.x, this.y, this.size, this.size);
         this.y += distance;
         this.draw();
     }
-    else if (dir == "right" && this.x + distance < canvas.clientWidth) {
-        ctx.clearRect(this.x, this.y, 10, 10);
+    else if (dir == "right" && this.x + distance + 10 < canvas.width) {
+        ctx.clearRect(this.x, this.y, this.size, this.size);
         this.x += distance;
         this.draw();
     }
     else if (dir == "left" && this.x - distance > 0) {
-        ctx.clearRect(this.x, this.y, 10, 10);
+        ctx.clearRect(this.x, this.y, this.size, this.size);
         this.x -= distance;
         this.draw();
     }
 }
 
-var draw_elf = function () {  
-    ctx.drawImage(elf,this.x ,this.y , 10, 10);
+const draw_elf = function () {  
+    ctx.drawImage(elf.img,this.x ,this.y , this.size, this.size);
 }
-var elf = new Image();
-elf.src = "src\\elf_right.png";
-var elf_val = {
-    x : 10,
-    y : 10,
+
+var elf = {
+    img : new Image(),
+    size : 10,
+    Direction : "right",
+    x : 150,
+    y : 75,
     draw : draw_elf,
     move : move
 };
 
+elf.img.src = "src\\elf_right.png";
 
-
-var elf_Direction = "right";
-
-var interval = function () { 
-    var speed = 1;
-    elf_val.move(elf_Direction, speed);
+const interval = function () {
+    const speed = 1;
+    elf.move(elf.Direction, speed);
 }
 
 setInterval(interval, 25);
 
 $("body").keydown(function (e) { 
-    elf_Direction = keycode[e.keyCode];
-    elf.src = "src\\elf_" + keycode[e.keyCode] + ".png"
+    elf.Direction = keycode[e.keyCode];
+    elf.img.src = "src\\elf_" + keycode[e.keyCode] + ".png"
 });
